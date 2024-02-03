fbbed25b65653db587782954bf3f2f11e74c982d
Irfansrii <irfanansari3227@gmIL.COM>
2024-02-03 10:43:19 +0000

    Faq App

---
+++ src/components/Faqs/index.js
@@ -0,0 +1,19 @@
+import FaqItem from '../FaqItem'
+import './index.css'
+
+const Faqs = props => {
+  const {faqsList} = props
+  return (
+    <div className="container">
+      <div className="sub-container">
+        <h1 className="heading">FAQs</h1>
+        <ul className="faqs-list">
+          {faqsList.map(eachFaq => (
+            <FaqItem key={eachFaq.id} faqDetails={eachFaq} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+export default Faqs
